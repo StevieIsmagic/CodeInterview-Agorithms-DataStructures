767d6df1f4a4207324519d07b5911c69d20ea4a9
Colombianito66 <colombianito66@gmail.com>
2018-09-29 23:55:04 -0400

    fibonacci memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,19 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+
+function memoizedFibonacci(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
 
 module.exports = fib;
